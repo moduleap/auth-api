ba0e12cc07373d871b073eb1be34c1f29acde098
PreCodeEU <lukas@lukasgermerott.de>
2019-04-08 13:36:29 +0200

    Updated lombok

--- src/main/java/org/moduleap/api/ApiApplication.java
+++ src/main/java/org/moduleap/api/ApiApplication.java
@@ -2,8 +2,7 @@ package org.moduleap.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
